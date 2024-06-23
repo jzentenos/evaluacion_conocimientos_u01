7eabb96ab8129a9481919b48ffb3a31ccdd8ff37
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-22 20:55:52 -0500

    Añadido menú de opciones y verificacion de denominador distinto de 0

--- calculadora_basica.cpp
+++ calculadora_basica.cpp
@@ -1,27 +1,41 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int num1,num2, r;
-	char op;
-	cout<<"Numero1: "; cin>>num1;
-	cout<<"Operador: "; cin>>op;
-	cout<<"Numero2: "; cin>>num2;
+	int num1,num2, r,op;
+	cout<<"Ingrese un numero: "; cin>>num1;
+	cout<<"Ingrese otro numero: "; cin>>num2;
+	cout<<"\tMenu de Opciones"<<endl;
+	cout<<"1. Sumar"<<endl;
+	cout<<"2. Restar"<<endl;
+	cout<<"3. Producto"<<endl;
+	cout<<"4. Division"<<endl;
+	cout<<"0. Salir"<<endl;
+	cout<<"Opcion: "; cin>>op;
+	
 	switch(op){
-		case '+':	
-				r =num1+num2;	
-		break;
-		case '-': 
-				r=num1-num2;
-		break;
-		case  '*':
-				r=num1*num2;
-		break;
-		case '/': 
-				r=num1/num2;
-		break;
+		case 1: r=num1+num2;
+			cout<<"Resultado: "<<r;
+			break;
+		case 2: r=num1-num2;
+			cout<<"Resultado: "<<r;	
+			break;
+		case 3: r=num1*num2;
+			cout<<"Resultado: "<<r;
+			break;
+		case 4: 
+				if(num2==0){
+					cout<<"Indeterminado";
+				}
+				else{
+					r=num1/num2;
+					cout<<"Resultado: "<<r;
+				}
+			break;
+		case 0: 
+			break;
 		default: 
-		break;
+				cout<<"Opcion no valida";
 	}
-	cout<<r;
+	
 	return 0;
 }
