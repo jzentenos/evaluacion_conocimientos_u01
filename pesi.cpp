5a391c8357e3f5e7a43aad2b6c4baf4badfc10eb
jzentenos <jzentenos@unjbg.edu.pe>
2024-06-22 21:58:22 -0500

    Añadido verificacion del numero menor y el ingreso de numeros positivos

--- pesi.cpp
+++ pesi.cpp
@@ -2,20 +2,39 @@
 using namespace std;
 int main(){
 	int n1,n2,c=0;
-	cout<<"Ingrese un numero: "; cin>>n1;
-	cout<<"Ingrese otro numero: "; cin>>n2;
+	int menor;
+	cout<<"\tPrograma Numeros PESI"<<endl;
+	cout<<"Numero 1: "; cin>>n1;
+	cout<<"Numero 2: "; cin>>n2;
+	if ( n1<=0 || n2<=0){
+		cout<<"\nIngrese ambos numeros positivos";
+		return 0;
+	}
+
+	if(n1<=n2){
+		if(n1<n2){
+			menor=n1;
+		}
+		else{
+			cout<<"Los numeros "<<n1<<" y "<<n2<<"No son PESI";
+		}
+	}
+	else{
+		menor=n2;
+	}
+	
 	int i=1;
-	while(i<=n1 && i<=n2){
+	while(i<=menor){
 		if(n1%i==0 && n2%i==0){
 			c++;
 		}
 		i++;
 	}
 	if(c>1){
-		cout<<"No son PESI";
+		cout<<"Los numeros "<<n1<<" y "<<n2<<" no son PESI";
 	}
 	else{
-		cout<<"Son PESI";
+		cout<<"Los numeros "<<n1<<" y "<<n2<<" son PESI";
 	}
 	return 0;
 }
